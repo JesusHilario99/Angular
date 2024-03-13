6694a4d7a8d818796b07ce22ff92f217b0cbedce
JesusHilario99 <hilario.juort99@gmail.com>
2024-03-13 16:49:44 -0500

    primer cambio en el git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mi-primera-app';
+  title = 'Mi primera app en angular perro!!';
+  mensaje = "Hola Guapo, otra variable";
 }
